8e74791da683b6b5cb03f9ce345f52507d17d045
Usong Cho <silvermouse4012@gmail.com>
2019-02-16 19:45:24 -0800

    functionality

--- app.js
+++ app.js
@@ -7,23 +7,32 @@ let budgetController = (function() {
 // UI CONTROLLER
 let uiController = (function() {
 
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector('.add__type').value,
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value,
+            };       
+        },
+    };
 })();
 
 // GLOBAL CONTROLLER
 let controller= (function(budgetCtrl, uiCtrl) {
 
-    document.querySelector('.add__btn').addEventListener('click', () => {
-        let inputValue = document.querySelector('.add__value').value;
-        
-    });
+    let ctrlAddItem = function() {
+        let input = uiCtrl.getInput();
+        console.log(input);
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
     
     document.addEventListener('keypress', (e) => {
-        let keyCode = e.keyCode;
-        let inputValue = document.querySelector('.add__value').value;
-        if (keyCode === 13) {
-            console.log(inputValue);
-        }
-    })
+        if (e.keyCode === 13) {
+            ctrlAddItem();
+        };
+    });
 
 })(budgetController, uiController);
 
