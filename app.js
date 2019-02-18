801e2680dab1450d8eb4fb993d2a599530edd6a9
Usong Cho <silvermouse4012@gmail.com>
2019-02-17 19:51:18 -0800

    input to our data structure

--- app.js
+++ app.js
@@ -15,16 +15,44 @@ let budgetController = (function() {
 
     let data = {
         allItems: {
-            exp: [],
-            inc: [],
+            expense: [],
+            income: [],
         },
         totals: {
-            exp: 0,
-            inc: 0,
+            expense: 0,
+            income: 0,
+        },
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            let newItem, ID;
+            let dataLength = data.allItems[type].length - 1;
+            if (dataLength + 1 > 0) {
+                // Create new Id
+                ID = data.allItems[type][dataLength].id + 1;
+            } else {
+                ID = 0;
+            };
+            // Create new Item based on 'expense' or 'Income'
+            if (type === 'expense') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'income') {
+                newItem = new Income(ID, des, val);
+            };
+            // Push it in to our data structure;
+            data.allItems[type].push(newItem);
+            return newItem;
+        },
+        returnData: function() {
+            console.log(data);
         },
     };
     
+    
 })();
+
+
 
 // UI CONTROLLER
 let uiController = (function() {
@@ -66,6 +94,8 @@ let controller= (function(budgetCtrl, uiCtrl) {
     
     let ctrlAddItem = function() {
         let input = uiCtrl.getInput();
+        let newItem = budgetCtrl.addItem(input.type, input.description, input.value)
+
     };
 
     return {
