f029cede9b596485ca3a5cc78c60f4494a9bc61f
Usong Cho <silvermouse4012@gmail.com>
2019-03-07 20:46:04 -0800

    practice

--- app.js
+++ app.js
@@ -196,3 +196,60 @@
 
 // controller.init();
 
+
+
+// let budgetController = (function() {
+
+// })();
+
+// let uiController = (function() {
+
+//     let DomStrings = {
+//         inputType: '.add__type',
+//         inputDescription: '.add__description',
+//         value: '.add__value',
+//         inputBtn: '.add__btn',
+//     };
+
+//     return {
+//         getInput: function() {
+//             return {
+//                  type: document.querySelector(DomStrings.inputType).value,
+//                  description: document.querySelector(DomStrings.inputDescription).value,
+//                  value: document.querySelector(DomStrings.value).value,
+//             }
+//         },
+//         getDomStrings: function() {
+//             return DomStrings;
+//         }
+//     };
+
+// })();
+
+// let controller = (function(budgerCtrl, uiCtrl) {
+
+//     let setupEventListeners = function() {
+//         let dom = uiCtrl.getDomStrings();
+//         document.querySelector(dom.inputBtn).addEventListener('click', ctrlAddItem);
+//         document.addEventListener('keypress', (e) => {
+//             if (e.keyCode === 13 || e.which === 13) {
+//                 ctrlAddItem();
+//             };
+//         });
+//     }
+
+//     let ctrlAddItem = function() {
+//         let input = uiCtrl.getInput();
+//         console.log(input);
+//     };
+
+//     return {
+//         init: function() {
+//             setupEventListeners();
+//         },
+//     };
+    
+
+// })(budgetController, uiController);
+
+// controller.init();
