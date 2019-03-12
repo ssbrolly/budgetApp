d47aeb905501fd9c4f3d9db7e34eeb22c269cfff
Usong Cho <silvermouse4012@gmail.com>
2019-03-11 20:28:49 -0700

    practice

--- app.js
+++ app.js
@@ -200,57 +200,104 @@
 
 // let budgetController = (function() {
 
-// })();
+//     let Expense = function(id, description, value) {
+//         this.id = id;
+//         this.description = description;
+//         this.value = value;
+//     };
 
-// let uiController = (function() {
+//     let Income = function(id, description, value) {
+//         this.id = id;
+//         this.description = description;
+//         this.value = value;
+//     };
 
-//     let DomStrings = {
-//         inputType: '.add__type',
-//         inputDescription: '.add__description',
-//         value: '.add__value',
-//         inputBtn: '.add__btn',
+//     let data = {
+//         allItems: {
+//             expense: [],
+//             income: [],
+//         },
+//         total: {
+//             expense: 0,
+//             income: 0,
+//         },
 //     };
 
 //     return {
-//         getInput: function() {
-//             return {
-//                  type: document.querySelector(DomStrings.inputType).value,
-//                  description: document.querySelector(DomStrings.inputDescription).value,
-//                  value: document.querySelector(DomStrings.value).value,
-//             }
-//         },
-//         getDomStrings: function() {
-//             return DomStrings;
-//         }
-//     };
-
-// })();
-
-// let controller = (function(budgerCtrl, uiCtrl) {
-
-//     let setupEventListeners = function() {
-//         let dom = uiCtrl.getDomStrings();
-//         document.querySelector(dom.inputBtn).addEventListener('click', ctrlAddItem);
-//         document.addEventListener('keypress', (e) => {
-//             if (e.keyCode === 13 || e.which === 13) {
-//                 ctrlAddItem();
+//         addItem: function(type, des, val) {
+//             let newItem, Id, idLength;
+//             idLength = data.allItems[type].length;
+//             if (idLength > 0) {
+//                 Id = data.allItems[type][idLength - 1].id + 1;
+//             } else {
+//                 Id = 0;
 //             };
-//         });
-//     }
-
-//     let ctrlAddItem = function() {
-//         let input = uiCtrl.getInput();
-//         console.log(input);
-//     };
-
-//     return {
-//         init: function() {
-//             setupEventListeners();
+//             if (type === 'expense') {
+//                 newItem = new Expense(Id, des, val);
+//             } else if ( type === 'income') {
+//                 newItem = new Income(Id, des, val);
+//             };
+//             data.allItems[type].push(newItem);
+//             return newItem;
+//         },
+//         testing: function() {
+//             console.log(data);
 //         },
 //     };
+     
+// })();
+
+
+let uiController = (function() {
+
+    let DomStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        value: '.add__value',
+        inputBtn: '.add__btn',
+    };
+
+    return {
+        getInput: function() {
+            return {
+                 type: document.querySelector(DomStrings.inputType).value,
+                 description: document.querySelector(DomStrings.inputDescription).value,
+                 value: document.querySelector(DomStrings.value).value,
+            }
+        },
+        getDomStrings: function() {
+            return DomStrings;
+        }
+    };
+
+})();
+
+let controller = (function(budgetCtrl, uiCtrl) {
+
+    let setupEventListeners = function() {
+        let dom = uiCtrl.getDomStrings();
+        document.querySelector(dom.inputBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', (e) => {
+            if (e.keyCode === 13 || e.which === 13) {
+                ctrlAddItem();
+            };
+        });
+    }
+
+    let ctrlAddItem = function() {
+        let input = uiCtrl.getInput();
+        let newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        console.log(input);
+    };
+
+    return {
+        init: function() {
+            setupEventListeners();
+        },
+    };
     
 
-// })(budgetController, uiController);
+})(budgetController, uiController);
 
-// controller.init();
+controller.init();
 
